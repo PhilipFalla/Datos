5d90e77b02cd0e2e903ff2783817c7fd2a428fcd
PhilipFalla <philipfalla@ufm.edu>
2025-02-19 10:07:05 -0600

    Queues
    Copia de versiones de tortola de queues. Haré mi versión en el futuro.

--- Complejidad/Busqueda_lineal.py
+++ Complejidad/Busqueda_lineal.py
@@ -1,5 +1,11 @@
 
 
+from memory_profiler import profile
+
+from time import time
+
+@profile
+
 def linear_search(arr, key): #receives an array and a value to search for
     
     for i in range(len(arr)): #for every element in the array, see if it's the value
@@ -16,4 +22,11 @@ test_n = 100
 test_arr = [7] * test_n
 test_key = 8
 
+start_time = time()  # Record the start time
+result = linear_search(test_arr, test_key)  # Call the function
+end_time = time()  # Record the end time
+
+# Output results
+print(f"Result: {result}")
+print(f"Execution Time: {end_time - start_time:.6f} seconds")
 print(linear_search(test_arr, test_key)) 
