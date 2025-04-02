3049684c644ed4f868a7fe9614254c87e46f0298
PhilipFalla <philipfalla@ufm.edu>
2025-04-02 09:19:28 -0600

    Added Binary tree and bst

--- Estructuras/binary_tree/binary_tree.py
+++ Estructuras/binary_tree/binary_tree.py
@@ -5,7 +5,7 @@ class Node:
         self.left_child = None
 
     def __repr__(self):
-        return '({}) <- |({})| -> ({})'.format(self.left_child, self.data, self.right_child)
+        return '( {} ) <- |({})| -> ( {} )'.format(self.left_child, self.data, self.right_child)
 
 class BinaryTree:
     def __init__(self):
